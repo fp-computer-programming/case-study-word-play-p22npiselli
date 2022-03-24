9699dc9a68bfb66829c1994860128ff42c1236a1
Nolan Piselli <p22npiselli@fairfieldprep.org>
2022-03-23 21:26:00 -0400

    word play

---
+++ word_play-1.py
@@ -0,0 +1,16 @@
+# Nolan Piselli
+
+# open the file and creates "file.txt"
+words = open("words.txt")
+file = open("file.txt","a")
+
+word = words.readlines()
+for x in word:
+# Interate through the words list 
+
+    if len(x.strip()) > 20: # remove the escape characters and checks if the word is > 20
+        file.write(x)
+
+# closes files
+words.close()
+file.close()
